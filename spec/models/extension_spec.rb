b425a0c60b7e396654b51cc295c5981fe0f57be8
vagrant <vagrant@vagrant.vm>
2018-04-21 22:44:39 +0000

    missed a test update

--- spec/models/extension_spec.rb
+++ spec/models/extension_spec.rb
@@ -15,7 +15,7 @@ describe Extension do
       expect(original.contract_terms).to eq("2G-2016")
       expect(original.acquired_cost).to eq(3)
       expect(original.notes).to eq("2016: Extended from 2G-2016 ($3 initial) to 3L-2018 ($4/yr); 2017: Stashed; 2018: HOLDING OUT ($34) []")
-      expect(contract.extension.original_contract.salary_schedule).to eq({ 2015 => 3, 2016 => 4 })
+      expect(SalaryCalculator.new(original).call).to eq({ 2015 => 3, 2016 => 4 })
     end
   end 
 end
