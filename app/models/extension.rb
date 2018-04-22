df5bfc6ede4e3466062ba7c5b75d4c88b59d6b96
vagrant <vagrant@vagrant.vm>
2018-04-22 02:42:09 +0000

    DRY extension

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -8,30 +8,34 @@ class Extension
 
   def extended_contract
     new_acquired_cost = SalaryCalculator.new(original_contract).call[original_contract.last_year]
-    updated_notes = contract.notes.remove("Extended:#{year}:#{original_terms};")
-    updated_notes = updated_notes.remove("Extended:#{year}:#{original_terms}")
-    updated_notes = updated_notes.remove("[]")
+
     extension = Contract.new(player: contract.player,
                              franchise: contract.franchise,
                              acquired_cost: new_acquired_cost,
                              contract_terms: contract.years_long.to_s + contract.type[0] + "-" + contract.last_year.to_s,
-                             notes: updated_notes)
+                             notes: clean_notes)
     extension.franchise.league = contract.franchise.league
     extension
   end
 
   def original_contract
-    updated_notes = contract.notes.remove("Extended:#{year}:#{original_terms};")
-    updated_notes = updated_notes.remove("Extended:#{year}:#{original_terms}")
-    updated_notes = updated_notes.remove("[]")
     original = Contract.new(player: contract.player,
                              franchise: contract.franchise,
                              acquired_cost: contract.acquired_cost,
 			     contract_terms: original_terms.remove("E"),
-                             notes: updated_notes)
+                             notes: clean_notes)
     original.franchise.league = contract.franchise.league
     original.events.delete_if {|e| e.year >= original.last_year }
     original
+  end
+
+  private
+
+  def clean_notes
+    updated_notes = contract.notes.remove("Extended:#{year}:#{original_terms};")
+    updated_notes = updated_notes.remove("Extended:#{year}:#{original_terms}")
+    updated_notes = updated_notes.remove("[]")
+    updated_notes
   end
 end
 
