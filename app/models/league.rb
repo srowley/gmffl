a82697da1d7afecb9f6136df0c825a69b0dcca1f
vagrant <vagrant@vagrant.vm>
2018-05-04 16:17:36 +0000

    revert back to fake contracts file

--- app/models/league.rb
+++ app/models/league.rb
@@ -8,8 +8,8 @@ class League
   def initialize(id = nil, year = nil)
     self.id = id
     self.year = year
-    self.roster_url = "http://www61.myfantasyleague.com/#{year}/export?TYPE=rosters&L=#{id}"
-#    self.roster_url = "#{Rails.root}/spec/fixtures/files/rosters_with_notes.xml"
+#    self.roster_url = "http://www61.myfantasyleague.com/#{year}/export?TYPE=rosters&L=#{id}"
+    self.roster_url = "#{Rails.root}/spec/fixtures/files/rosters_with_notes.xml"
     self.players_url = "https://www70.myfantasyleague.com/#{year}/export?TYPE=players&DETAILS=&SINCE=&PLAYERS=&JSON=0"
     self.franchises_url = "https://www61.myfantasyleague.com/#{year}/export?TYPE=league&L=#{id}"
     self.adjustments_url = "https://www61.myfantasyleague.com/#{year}/export?TYPE=salaryAdjustments&L=#{id}"
